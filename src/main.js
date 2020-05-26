fbce3336117304a7a190526439f5f6f634b10a07
Shan <shan.mi.fanfan@gmail.com>
2020-05-26 12:09:00 +0200

    refactor

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 const rootURL = "https://swapi.dev/api/people/?page=";
-const totalPage = 9;
 const searchBtn = document.querySelector("button");
 const charactersArea = document.querySelector(".characters-wrapper");
 const searchTermArea = document.querySelector("#searchTerm");
-let id = 0;
-let characters,
+let id = 0,
+  characters,
   i = 1,
   charactersJson,
-  results = [];
+  results = [],
+  filterResults = [];
 
 const getCharacters = async () => {
   let isThisEnd = false;
@@ -23,45 +23,44 @@ const getCharacters = async () => {
       getCharacters();
     }
     if (!charactersJson.next) {
-      console.log("dead end");
+      // console.log("dead end");
       isThisEnd = true;
     }
   } catch (err) {
     console.log(err);
   }
 
-  console.log(i);
   if (isThisEnd) {
     drawCharacterArea(results);
+    // console.log(results);
   }
 };
+
 getCharacters();
-console.log(results);
 
 function drawCharacterArea(results) {
-  results.forEach((result) => {
-    generateCharatersPanel(result);
-  });
-  searchBtn.addEventListener("keyup", (e) => {
+  generateCharatersPanel(results);
+
+  searchTermArea.addEventListener("keyup", (e) => {
+    // console.log(e.target.value.toLowerCase());
+    // console.log(results);
     const searchString = e.target.value.toLowerCase();
-
-    const searchResults = results.filter((result) =>
-      result.name.toLowerCase().includes(searchString)
-    );
-    showSelectCharactor(searchResults);
+    let isThereAnyFound = false;
+    filterResults = [];
+    results.forEach((result) => {
+      if (result.name.toLowerCase().includes(searchString)) {
+        // console.log(results.indexOf(result));
+        // console.log(result);
+        filterResults.push(result);
+        // console.log(filterResults);
+        isThereAnyFound = true;
+        generateCharatersPanel(filterResults);
+      } else if (!isThereAnyFound) {
+        charactersArea.innerHTML = "";
+        // console.log("no found");
+      }
+    });
   });
-  // searchBtn.onclick = showSelectCharactor;
-
-  function showSelectCharactor(e) {
-    e.preventDefault();
-    let id = 0;
-    charactersArea.innerHTML = "";
-    // let searchKeyWords = searchTermArea.value.toLowerCase();
-    // const searchResults = results.filter((result) => {
-    //   result.name.toLowerCase().includes(searchKeyWords);
-    // });
-    drawCharacterArea(searchResults);
-  }
 }
 
 const swPanel = document.querySelector("#sw-panel");
@@ -76,51 +75,60 @@ swPanel.addEventListener("click", (e) => {
 
 document.querySelector(".date").innerHTML = new Date();
 
-function generateCharatersPanel(character) {
-  let films = "";
-  let gender;
-  let hrefLink;
+function generateCharatersPanel(characters) {
+  charactersArea.innerHTML = "";
+  id = 0;
+  const htmlString = characters
+    .map((character) => {
+      let films = "";
+      let gender;
+      let hrefLink;
 
-  character.films.map((film) => {
-    switch (film.slice(-2, -1)) {
-      case "1":
-        hrefLink =
-          "https://en.wikipedia.org/wiki/Star_Wars:_Episode_I_%E2%80%93_The_Phantom_Menace";
-        break;
-      case "2":
-        hrefLink =
-          "https://en.wikipedia.org/wiki/Star_Wars:_Episode_II_%E2%80%93_Attack_of_the_Clones";
-        break;
-      case "3":
-        hrefLink =
-          "https://en.wikipedia.org/wiki/Star_Wars:_Episode_III_%E2%80%93_Revenge_of_the_Sith";
-        break;
-      case "4":
-        hrefLink = "https://en.wikipedia.org/wiki/Star_Wars:_The_Force_Awakens";
-        break;
-      case "5":
-        hrefLink = "https://en.wikipedia.org/wiki/Star_Wars:_The_Last_Jedi";
-        break;
-      case "6":
-        hrefLink =
-          "https://en.wikipedia.org/wiki/Star_Wars:_The_Rise_of_Skywalker";
-        break;
-    }
-    return (films += `<a href='${hrefLink}'>${film}</a>`);
-  });
-  gender = character.gender.replace(/\//, "");
+      character.films.map((film) => {
+        switch (film.slice(-2, -1)) {
+          case "1":
+            hrefLink =
+              "https://en.wikipedia.org/wiki/Star_Wars:_Episode_I_%E2%80%93_The_Phantom_Menace";
+            break;
+          case "2":
+            hrefLink =
+              "https://en.wikipedia.org/wiki/Star_Wars:_Episode_II_%E2%80%93_Attack_of_the_Clones";
+            break;
+          case "3":
+            hrefLink =
+              "https://en.wikipedia.org/wiki/Star_Wars:_Episode_III_%E2%80%93_Revenge_of_the_Sith";
+            break;
+          case "4":
+            hrefLink =
+              "https://en.wikipedia.org/wiki/Star_Wars:_The_Force_Awakens";
+            break;
+          case "5":
+            hrefLink = "https://en.wikipedia.org/wiki/Star_Wars:_The_Last_Jedi";
+            break;
+          case "6":
+            hrefLink =
+              "https://en.wikipedia.org/wiki/Star_Wars:_The_Rise_of_Skywalker";
+            break;
+        }
+        return (films += `<a href='${hrefLink}'>${film}</a>`);
+      });
+      gender = character.gender.replace(/\//, "");
+      id++;
 
-  charactersArea.innerHTML += `
-    <div data-id="${id}" class="character">
-      <div class="name-container ${gender}">
-        <a class='showInfo'><p class="name" data-id="${id}">${character.name}</p></a>
-      </div>
-      <div class="info-container hidden" data-info="${id}">
-        ${films} 
-        <p class="text"><b>Hair Color</b>: ${character.hair_color}</p>
-        <p class="text"><b>Eye Color</b>: ${character.eye_color}</p>
-      </div>
-    </div>
-    `;
-  id++;
+      return `
+        <div data-id="${id}" class="character">
+          <div class="name-container ${gender}">
+            <a class='showInfo'><p class="name" data-id="${id}">${character.name}</p></a>
+          </div>
+          <div class="info-container hidden" data-info="${id}">
+            ${films} 
+            <p class="text"><b>Hair Color</b>: ${character.hair_color}</p>
+            <p class="text"><b>Eye Color</b>: ${character.eye_color}</p>
+          </div>
+        </div>
+        `;
+    })
+    .join("");
+
+  charactersArea.innerHTML += htmlString;
 }
